9541f252193ee32355b5e6cc579456d5e6949536
Vitor Giovane <vitorgpalves@gmail.com>
2019-04-26 12:24:32 -0300

    feature(class4): uses url params and query params

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  return res.send('Hello, World!')
+  return res.send(`Welcome, ${req.query.name}!`)
 })
 
-app.get('/login', (req, res) => {
-  return res.send('Login')
+app.get('/name/:name', (req, res) => {
+  return res.send(`Welcome, ${req.params.name}!`)
 })
 
 app.listen(3000)
