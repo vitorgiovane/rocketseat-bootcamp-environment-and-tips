52232c18b746e7752abf5ad3bb5879294d7b1d23
Vitor <vitorgpalves@gmail.com>
2019-04-26 22:22:29 -0300

    feature(class10): creates the user registration form

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ nunjucks.configure('views', {
   watch: true
 })
 
+app.use(express.urlencoded({ extended: false }))
+
 app.set('view engine', 'njk')
 
 const users = ['Vitor Giovane', 'Diego Fernandes', 'Filipe Deschamps']
@@ -19,6 +21,11 @@ app.get('/', (req, res) => {
 
 app.get('/new', (req, res) => {
   return res.render('new')
+})
+
+app.post('/create', (req, res) => {
+  users.push(req.body.user)
+  return res.redirect('/')
 })
 
 app.listen(3000)
