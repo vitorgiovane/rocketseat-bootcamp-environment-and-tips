18d5e723f221286696f315a37b4ded0931806356
Vitor <vitorgpalves@gmail.com>
2019-04-26 22:02:17 -0300

    feature(class8): configures a template nunjucks

--- index.js
+++ index.js
@@ -1,29 +1,18 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
 
 const app = express()
 
-const logMiddleware = (req, res, next) => {
-  console.log(`
-  HOST: ${req.headers.host}
-  URL: ${req.url}
-  METHOD: ${req.method}
-  `)
-
-  req.appName = 'GoNode'
-  
-  return next()
-}
-
-app.use(logMiddleware)
-
-app.get('/', (req, res) => {
-  return res.send(`Welcome to ${req.appName}, ${req.query.name}!`)
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app,
+  watch: true
 })
 
-app.get('/name/:name', (req, res) => {
-  return res.json({
-    message: `Welcome to ${req.appName}, ${req.params.name}!`
-  })
+app.set('view engine', 'njk')
+
+app.get('/', (req, res) => {
+  return res.render('list', { name: "Vitor" })
 })
 
 app.listen(3000)
